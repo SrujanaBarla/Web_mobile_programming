82046050ac9b2f24e965a9ce9498e0ab6543ac6a
SrujanaBarla <89660593+SrujanaBarla@users.noreply.github.com>
2021-09-20 03:54:35 -0500

    a

--- ICP4/Source/Task1/hover.js
+++ ICP4/Source/Task1/hover.js
@@ -1,11 +1,5 @@
 function upDate(previewPic) {
-    /* In this function you should
-   1) change the url for the background image of the div with the id = "image"
-   to the source file of the preview image
 
-   2) Change the text  of the div with the id = "image"
-   to the alt text of the preview image
-   */
     const images = previewPic.getAttribute( "src" );
     const words = previewPic.getAttribute( "alt" );
     document.getElementById('image').style.backgroundImage="url('" + images + "')";
@@ -15,13 +9,7 @@ function upDate(previewPic) {
 }
 
 function unDo() {
-    /* In this function you should
-      1) Update the url for the background image of the div with the id = "image"
-      back to the original-image.  You can use the css code to see what that original URL was
 
-      2) Change the text  of the div with the id = "image"
-      back to the original text.  You can use the html code to see what that original text was
-    */
     document.getElementById('image').innerText= "Hover over an image below to display here.";
     document.getElementById('image').style.background='peachpuff';
 }
